a386a88526bb833b4cbd453ae29b15ce05c8c6ff
Ramprasad35 <ramprasadsk2020@gmail.com>
2026-03-24 18:01:15 +0530

    integrated hugging face

--- src/VectorDB.py
+++ src/VectorDB.py
@@ -17,7 +17,7 @@ def save_index(index,path="faiss_index_bin"):
     faiss.write_index(index,path)
 
 def load_index(index,path="faiss_index_bin"):
-    return faiss.read_index(index,path)
+    return faiss.read_index(index,path) 
 
 def save_chunks(chunks,path="chunks.pkl"):
     with open(path,"wb") as f:
--- src/embedding.py
+++ src/embedding.py
@@ -1,5 +1,5 @@
 from sentence_transformers import SentenceTransformer
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("all-MiniLM-L6-v2",trust_remote_code = True)
 def get_embedding(chunks):
     return model.encode("chunks")
 def query_embedding(query):
